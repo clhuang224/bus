15d95bcdb03b82f412c8f0cf483730c96d937dd2
Lynn <clhuang224@gmail.com>
2025-07-24 12:19:07 +0800

    fix(eslint): workaround for missing flat config support in eslint-plugin-react@7

--- eslint.config.js
+++ eslint.config.js
@@ -5,21 +5,59 @@ import pluginReact from 'eslint-plugin-react'
 import json from '@eslint/json'
 import { defineConfig } from 'eslint/config'
 
-
 export default defineConfig([
-  { files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'], plugins: { js }, extends: ['js/recommended'] },
-  { files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'], languageOptions: { globals: {...globals.browser, ...globals.node} } },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  pluginReact.configs.flat['jsx-runtime'],
   {
+    ignores: ['./build/**', './node_modules/**', './.react-router/**']
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    plugins: {
+      js,
+      react: pluginReact
+    },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      },
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
+    },
+    rules: {
+      'react/display-name': 'off',
+      'react/jsx-key': 'warn',
+      'react/jsx-no-comment-textnodes': 'warn',
+      'react/jsx-no-duplicate-props': 'warn',
+      'react/jsx-no-target-blank': 'warn',
+      'react/jsx-uses-react': 'warn',
+      'react/jsx-uses-vars': 'warn',
+      'react/no-children-prop': 'warn',
+      'react/no-danger-with-children': 'warn',
+      'react/no-deprecated': 'warn',
+      'react/no-direct-mutation-state': 'warn',
+      'react/no-unescaped-entities': 'warn',
+      'react/no-unknown-property': 'warn',
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off'
+    },
     settings: {
       react: {
         version: 'detect'
       }
     }
   },
-  { files: ['**/*.json'], plugins: { json }, language: 'json/json', extends: ['json/recommended'] },
+  tseslint.configs.recommended,
+  {
+    files: ['**/*.json'],
+    plugins: { json },
+    language: 'json/json',
+    extends: ['json/recommended']
+  },
   {
     rules: {
       semi: ['warn', 'never'],
@@ -27,4 +65,4 @@ export default defineConfig([
       quotes: ['warn', 'single', { avoidEscape: true }]
     }
   }
-]);
+])
