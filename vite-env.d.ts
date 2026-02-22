430adafe9feb3081f0c6617b6b81377ec5abf015
Lynn <clhuang224@gmail.com>
2026-02-22 22:32:28 +0800

    fix: remove unused ImportMetaEnv interface

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -4,12 +4,6 @@ interface ViteTypeOptions {
   strictImportMetaEnv: unknown
 }
 
-interface ImportMetaEnv {
-  readonly VITE_API_BASE: string
-  readonly VITE_TDX_ID: string
-  readonly VITE_TDX_SECRET: string
-}
-
 interface ImportMeta {
   readonly env: ImportMetaEnv
 }
