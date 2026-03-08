18b2ec931ee640a98568975244325e76b6f7621a
Lynn <clhuang224@gmail.com>
2026-03-09 01:56:11 +0800

    fix(routes): update route paths to remove leading slashes for consistency fix(router): set basename for production environment in router configuration

--- app/routes.ts
+++ app/routes.ts
@@ -3,8 +3,8 @@ import { type RouteConfig, route, index } from '@react-router/dev/routes'
 export default [
     route('/', './pages/AppLayout.tsx', [
         index('./pages/Favorite.tsx'),
-        route('/bus-route/:city/:id', './pages/BusRoute.tsx'),
-        route('/search', './pages/Search.tsx'),
-        route('/nearby', './pages/Nearby.tsx')
+        route('bus-route/:city/:id', './pages/BusRoute.tsx'),
+        route('search', './pages/Search.tsx'),
+        route('nearby', './pages/Nearby.tsx')
     ])
 ] satisfies RouteConfig
