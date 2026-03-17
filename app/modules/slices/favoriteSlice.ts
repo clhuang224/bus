89169cafee17737ba7c52167b48f00749523c8b1
Lynn <clhuang224@gmail.com>
2026-03-17 19:17:31 +0800

    fix(favorite): typecheck

--- app/modules/slices/favoriteSlice.ts
+++ app/modules/slices/favoriteSlice.ts
@@ -1,27 +1,27 @@
 import { createSlice, type PayloadAction } from '@reduxjs/toolkit'
-import type { NearStop } from '../interfaces/NearStop'
+import type { Stop } from '../interfaces/Stop'
 
 const favoriteSlice = createSlice({
   name: 'favorite',
   initialState: {
-    stops: [] as NearStop[]
+    stops: [] as Stop[]
   },
   reducers: {
     loadLocalStorage: (state) => {
-      state.stops = JSON.parse(localStorage.getItem('favoriteStops') ?? '[]') as NearStop[]
+      state.stops = JSON.parse(localStorage.getItem('favoriteStops') ?? '[]') as Stop[]
     },
-    setFavoriteStops: (state, action: PayloadAction<NearStop[]>) => {
+    setFavoriteStops: (state, action: PayloadAction<Stop[]>) => {
       state.stops = action.payload
       localStorage.setItem('favorite', JSON.stringify(state.stops))
     },
-    addFavoriteStop: (state, action: PayloadAction<NearStop>) => {
+    addFavoriteStop: (state, action: PayloadAction<Stop>) => {
       const newStop = action.payload
       if (!state.stops.map((stop) => stop.StopUID).includes(newStop.StopUID)) {
         state.stops.push(newStop)
         localStorage.setItem('favorite', JSON.stringify(state.stops))
       }
     },
-    removeFavoriteStop: (state, action: PayloadAction<NearStop>) => {
+    removeFavoriteStop: (state, action: PayloadAction<Stop>) => {
       const stopToRemove = action.payload
       state.stops = state.stops.filter((stop) => stop.StopUID === stopToRemove.StopUID)
       localStorage.setItem('favorite', JSON.stringify(state.stops))
