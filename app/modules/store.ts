33e3a52ea49ba596adfb8677770e2e5d46fceb7c
Lynn <clhuang224@gmail.com>
2026-03-20 00:39:40 +0800

    chore(store): enable RTK Query listeners
    Initialize RTK Query listeners in the Redux store.
    This supports query behaviors such as reconnect refetching and polling control while the app is unfocused.

--- app/modules/store.ts
+++ app/modules/store.ts
@@ -1,4 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { setupListeners } from '@reduxjs/toolkit/query'
 import { busApi } from './apis/bus'
 import favoriteSlice from './slices/favoriteSlice'
 import geoSlice from './slices/geoSlice'
@@ -7,23 +8,25 @@ import globalModalSlice from './slices/globalModalSlice'
 import routeSearchSlice from './slices/routeSearchSlice'
 
 export const store = configureStore({
-    reducer: {
-        [busApi.reducerPath]: busApi.reducer,
-        geolocation: geoSlice.reducer,
-        favorite: favoriteSlice.reducer,
-        cityGeo: cityGeoSlice.reducer,
-        globalModal: globalModalSlice.reducer,
-        routeSearch: routeSearchSlice.reducer
-    },
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware({
-            serializableCheck: {
-                warnAfter: 64,
-                ignoredActions: [setGeoJSON.type],
-                ignoredPaths: ['cityGeo.geojson', busApi.reducerPath]
-            }
-        }).concat(busApi.middleware)
+  reducer: {
+    [busApi.reducerPath]: busApi.reducer,
+    geolocation: geoSlice.reducer,
+    favorite: favoriteSlice.reducer,
+    cityGeo: cityGeoSlice.reducer,
+    globalModal: globalModalSlice.reducer,
+    routeSearch: routeSearchSlice.reducer
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        warnAfter: 64,
+        ignoredActions: [setGeoJSON.type],
+        ignoredPaths: ['cityGeo.geojson', busApi.reducerPath]
+      }
+    }).concat(busApi.middleware)
 })
+
+setupListeners(store.dispatch)
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
