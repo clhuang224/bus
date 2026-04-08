053af36d31b00289f5d757747955b13afe019f21
Lynn <clhuang224@gmail.com>
2026-04-08 23:11:05 +0800

    fix(types): update MessageType and add TripStartTimeType to interfaces

--- app/modules/interfaces/RouteRealtimeBusStatus.ts
+++ app/modules/interfaces/RouteRealtimeBusStatus.ts
@@ -1,13 +1,11 @@
 import type { DirectionType } from '../enums/DirectionType'
-import type { LngLat } from '../types/CoordsType'
 
 export interface RouteRealtimeBusStatus {
   direction: DirectionType
   estimateLabel: string
   estimateMinutes: number | null
   id: string
-  plateNumb: string | null
-  position: LngLat | null
+  plateNumb: string
   stopName: string
   stopSequence: number
   subRouteUID: string
