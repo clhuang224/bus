962386ef9013cee81328fef25d000511dd5e1b87
Lynn <clhuang224@gmail.com>
2026-04-10 23:46:00 +0800

    refactor(route): simplify vehicle state transformation logic

--- app/modules/utils/route/transformTdxVehicleState.ts
+++ app/modules/utils/route/transformTdxVehicleState.ts
@@ -1,11 +1,12 @@
 import { TdxA2EventType, VehicleStateType } from '../../enums/VehicleStateType'
 
 export function transformTdxVehicleState(a2EventType?: TdxA2EventType | null) {
-  if (a2EventType == null) {
-    return undefined
+  switch (a2EventType) {
+    case TdxA2EventType.DEPARTED:
+      return VehicleStateType.DEPARTED
+    case TdxA2EventType.ARRIVING:
+      return VehicleStateType.ARRIVING
+    default:
+      return undefined
   }
-
-  return a2EventType === TdxA2EventType.DEPARTED
-    ? VehicleStateType.DEPARTED
-    : VehicleStateType.ARRIVING
 }
