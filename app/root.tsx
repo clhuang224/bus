edc117cc1c4e0f87fb1675944218ec842324c7f6
Lynn <clhuang224@gmail.com>
2026-04-13 07:37:08 +0800

    fix(locale): update HTML lang attribute to use resolved language

--- app/root.tsx
+++ app/root.tsx
@@ -44,7 +44,7 @@ export function meta() {
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang={DEFAULT_APP_LOCALE}>
+    <html lang={i18n.resolvedLanguage ?? i18n.language ?? DEFAULT_APP_LOCALE}>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
